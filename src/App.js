2bbf2f67b14ef72b6ed594ca50178877955e5c32
Carlo Landicho <landichocarlo1@gmail.com>
2024-03-31 23:14:38 +0100

    My first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} id="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <code>Hello World </code>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <body className= "app-body">
+        <h1>Carlo</h1>  
+      </body>
+    </div>
+  );
+}
+
+export default App;
